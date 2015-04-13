652ebc0d9665e1faa38e0dea121bc7b6e802c3ab
胡正伟 <zwhu2014@foxmail.com>
2015-04-14 00:30:11 +0800

    优化 gulp 的文件， 通过命令行的 gui 文件，运行 tasks。

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,22 @@
 var gulp = require('gulp');
 
 // 引入组件
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var gutil = require('gulp-util');
-var rename = require('gulp-rename');
+var jshint     = require('gulp-jshint');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var gutil      = require('gulp-util');
+var rename     = require('gulp-rename');
 var browserify = require('browserify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream');
-var minifyCSS = require('gulp-minify-css');
-var del = require('del');
-var mocha = require('gulp-mocha');
+var reactify   = require('reactify');
+var source     = require('vinyl-source-stream');
+var minifyCSS  = require('gulp-minify-css');
+var del        = require('del');
+var mocha      = require('gulp-mocha');
 
+// gulp gui
+var gulpGui = require('./gulpgui.js');
 
 // TODO: live load
-
 // clean
 gulp.task('clean', function (cb) {
     del(['./public/build/', './public/javascripts/*.js', './public/stylesheets/style.min.css'], cb)
@@ -37,10 +38,13 @@ gulp.task('browserify', function () {
         entries: './www/views/app.jsx',
         transform: [reactify]
     }).bundle()
-        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-        //Pass desired output filename to vinyl-source-stream
+        .on('error', function (err) {
+            gutil.log(
+                gutil.colors.red("Browserify compile error: \n\t"),
+                err.message
+            );
+        })
         .pipe(source('bundle.js'))
-        // Start piping stream to tasks!
         .pipe(gulp.dest('./public/build/'));
 });
 
@@ -66,13 +70,13 @@ gulp.task('test', function () {
 });
 
 // 默认任务
+gulp.task('build', ['minify-js', 'minify-css']);
+
 gulp.task('default', function () {
-    gulp.run('minify-js', 'minify-css');
+    gulpGui(['test', 'dev', 'pro']);
 });
 
 gulp.task('watch', function () {
     // 监听文件变化
-    gulp.watch('./www/views/**/*.js*', function () {
-        gulp.run('browserify', 'concat');
-    });
+    gulp.watch('./www/views/**/*.js*', ['browserify', 'concat']);
 });
